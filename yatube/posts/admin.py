5f60b4c58b58a32f2cd247a9f9fbaffd906582c1
xplanet95 <vladis.box@yandex.ru>
2022-05-17 15:38:43 +0800

    --

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
-
-from .models import Post
+# из файла models импортируем модель Post
+from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
     # перечисляем поля, которые должны отображаться в админке
-    list_display = ("text", "pub_date", "author")
+    list_display = ("pk", "text", "pub_date", "author")
     # добавляем интерфейс для поиска по тексту постов
     search_fields = ("text",)
     # добавляем возможность фильтрации по дате
     list_filter = ("pub_date",)
+    # это свойство сработает для всех колонок: где пусто - там будет эта строка
+    empty_value_display = "-пусто-"
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "slug", "description")
+    search_fields = ("title",)
+    empty_value_display = "-пусто-"
 
 # при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
@@ -8,3 +7,15 @@ def index(request):
     context = {"posts": latest}
     response = render(request, "index.html", context)
     return response
+
+def group_posts(request, slug):
+    # функция get_object_or_404 получает по заданным критериям объект из базы данных
+    # или возвращает сообщение об ошибке, если объект не найден
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям. Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    #  posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    posts = group.posts.all().order_by("-pub_date")[:12]
+    context = {"group": group, "posts": posts}
+    return render(request, "group.html", context)
