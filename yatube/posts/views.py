e51e4f8ea9b8aff4c24aa82b35746ca6e6582ea6
xplanet95 <vladis.box@yandex.ru>
2022-05-18 14:09:02 +0800

    --

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,7 @@ from .models import Post, Group
 
 
 def index(request):
-    latest = Post.objects.order_by("-pub_date")[:11]
+    latest = Post.objects.all()[:11]
     context = {"posts": latest}
     response = render(request, "index.html", context)
     return response
