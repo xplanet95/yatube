0f03df634a1b0aeea221fe3b457850d7d42fcc0d
xplanet95 <vladis.box@yandex.ru>
2022-06-15 13:41:10 +0800

    paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, Group
 from .forms import PostForm
+from django.core.paginator import Paginator
 # from django.views.generic import CreateView
 # from django.urls import reverse_lazy
 
+
 def index(request):
-    latest = Post.objects.all()[:11]
-    context = {"posts": latest}
+    post_list = Post.objects.order_by('-pub_date').all()  # [:11]
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')  # переменная в URL с номером запрошенной страницы
+    page = paginator.get_page(page_number)  # получить записи с нужным смещением
+    context = {'page': page, 'paginator': paginator}
     response = render(request, "index.html", context)
     return response
 
@@ -15,12 +20,14 @@ def group_posts(request, slug):
     # функция get_object_or_404 получает по заданным критериям объект из базы данных
     # или возвращает сообщение об ошибке, если объект не найден
     group = get_object_or_404(Group, slug=slug)
-
     # Метод .filter позволяет ограничить поиск по критериям. Это аналог добавления
     # условия WHERE group_id = {group_id}
     #  posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
-    posts = group.posts.all().order_by("-pub_date")[:12]
-    context = {"group": group, "posts": posts}
+    post_list = group.posts.all().order_by("-pub_date").all()  # [:12]
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    context = {"group": group, 'page': page, 'paginator': paginator}
     return render(request, "group.html", context)
 
 
