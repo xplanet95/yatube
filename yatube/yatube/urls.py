e79a4fd960f0d072ac5e3a1bc4339a2a0052da2e
xplanet95 <vladis.box@yandex.ru>
2022-07-07 08:55:15 +0800

    profile url fix

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -7,22 +7,24 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    #  обработчик для главной страницы ищем в urls.py приложения posts
-    path("", include("posts.urls")),
+    #  раздел администратора
+    path("admin/", admin.site.urls),
+    # flatpages
+    path('about/', include('django.contrib.flatpages.urls')),
     #  регистрация и авторизация
     path("auth/", include("users.urls")),
     #  если нужного шаблона для /auth не нашлось в файле users.urls —
     #  ищем совпадения в файле django.contrib.auth.urls
     path("auth/", include("django.contrib.auth.urls")),
-    #  раздел администратора
-    path("admin/", admin.site.urls),
-    # flatpages
-    path('about/', include('django.contrib.flatpages.urls')),
 ]
 
 urlpatterns += [
         path('about-author/', views.flatpage, {'url': '/about-author/'}, name='about-author'),
         path('about-spec/', views.flatpage, {'url': '/about-spec/'}, name='about-spec'),
+]
+urlpatterns += [
+    #  обработчик для главной страницы ищем в urls.py приложения posts
+    path("", include("posts.urls")),
 ]
 
 handler404 = "posts.views.page_not_found" # noqa (от NO Quality Assurance)
